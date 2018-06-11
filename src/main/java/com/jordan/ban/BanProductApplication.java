f5fd18de9ab38e372fcf21637efe5068d600d508
liji <liji.jordan@gmail.com>
2018-06-11 11:02:31 +0800

    disable datasource

--- src/main/java/com/jordan/ban/BanProductApplication.java
+++ src/main/java/com/jordan/ban/BanProductApplication.java
@@ -7,6 +7,9 @@ import com.jordan.ban.service.AccountService;
 import com.jordan.ban.service.TradeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,7 +21,7 @@ import java.util.concurrent.ExecutionException;
 @EnableScheduling
 @ComponentScan("com.jordan.ban")
 @EnableJpaRepositories("com.jordan.ban.dao")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class BanProductApplication {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
