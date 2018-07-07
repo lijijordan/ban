428d812824432b9300670f6ea2535670c3fa80e1
liji <liji.jordan@gmail.com>
2018-07-07 17:30:24 +0800

    FIx fees

--- src/main/java/com/jordan/ban/BanProductApplication.java
+++ src/main/java/com/jordan/ban/BanProductApplication.java
@@ -21,21 +21,21 @@ public class BanProductApplication {
         ProductApplication productApplication = context.getBean(ProductApplication.class);
 
         // huobi vs dragonex
-        productApplication.diffTask(Constant.NEO_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.EOS_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.BTC_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.EOS_ETH, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.NEO_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.BTC_USDT, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_ETH, Huobi.PLATFORM_NAME, Dragonex.PLATFORM_NAME, 2000);
 
         // huobi vs gateio
-        productApplication.diffTask(Constant.EOS_BTC, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.EOS_ETH, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.EOS_USDT, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_BTC, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_ETH, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_USDT, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
 
         // fcoin vs huobi
-        productApplication.diffTask(Constant.BCH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.BTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.ETH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.LTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.BCH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.BTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.ETH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.LTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 
         // fcoin vs dragonex
         productApplication.diffTask(Constant.LTC_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
