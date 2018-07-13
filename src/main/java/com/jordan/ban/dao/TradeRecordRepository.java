0785c2ac0d6fec62efd56914e8f8730fd0603918
liji <liji.jordan@gmail.com>
2018-07-13 10:09:53 +0800

    no message

--- src/main/java/com/jordan/ban/dao/TradeRecordRepository.java
+++ src/main/java/com/jordan/ban/dao/TradeRecordRepository.java
@@ -1,15 +1,21 @@
 package com.jordan.ban.dao;
 
-import com.jordan.ban.entity.Account;
 import com.jordan.ban.entity.TradeRecord;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
 
 public interface TradeRecordRepository extends CrudRepository<TradeRecord, Long> {
 
     @Query(value = "SELECT AVG(eat_diff_percent) FROM trade_record WHERE eat_diff_percent>0 AND accountA=?1 AND accountB=?2 AND symbol=?3",
             nativeQuery = true)
     Double avgEatDiffPercent(long accountA, long accountB, String symbol);
+
+
+    @Query(value = "select * from trade_record where create_time>?1 order by create_time desc;", nativeQuery = true)
+    List<TradeRecord> findAllByCreateTime(Date createTime);
 
     int countBy();
 }
