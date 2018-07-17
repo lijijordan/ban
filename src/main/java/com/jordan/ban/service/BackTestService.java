cb412e002507ceec54ddfa2ee4e1017d7abe7a40
liji <liji.jordan@gmail.com>
2018-07-18 07:49:21 +0800

    no message

--- src/main/java/com/jordan/ban/service/BackTestService.java
+++ src/main/java/com/jordan/ban/service/BackTestService.java
@@ -543,8 +543,8 @@ public class BackTestService {
 
         this.policy = Policy.max;
 
-        Date start = format.parse("2018/07/14 00:00:00");
-        Date end = format.parse("2018/07/15 00:00:00");
+        Date start = format.parse("2018/07/10 00:00:00");
+        Date end = format.parse("2018/07/16 12:00:00");
 //        Date start = format.parse("2018/07/01 00:00:29");
 //        Date end = format.parse("2018/07/04 23:59:00");
 //        this.moveMetric = 0.02692;
@@ -602,6 +602,7 @@ public class BackTestService {
 //        run(start, end, 0.026f, -0.02f, 6000, 6000, 1, 0.1f, ETH_USDT);
 
 //        run(start, end, 12000, ETH_USDT);
+        this.split = 1; this.wareHouseDiff = 0.007;
         run(start, end, 6000, ETH_USDT);
         System.out.println("cycle times:" + this.countCycle);
         System.out.println("End at:" + new Date());
