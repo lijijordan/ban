e57f0e0ae021931799302b1ad86581ec391f4996
liji <liji.jordan@gmail.com>
2018-08-29 13:27:04 +0800

    add EOS_USDT.

--- src/main/java/com/jordan/ban/BanProductApplication.java
+++ src/main/java/com/jordan/ban/BanProductApplication.java
@@ -29,26 +29,28 @@ public class BanProductApplication {
         // huobi vs gateio
 //        productApplication.diffTask(Constant.EOS_BTC, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
 //        productApplication.diffTask(Constant.EOS_ETH, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.EOS_USDT, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.EOS_USDT, Gateio.PLATFORM_NAME, Huobi.PLATFORM_NAME, 2000);
 
         // fcoin vs huobi
 //        productApplication.diffTask(Constant.BCH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 //        productApplication.diffTask(Constant.BTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
-        productApplication.diffTask(Constant.ETH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.ETH_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 //        productApplication.diffTask(Constant.LTC_USDT, Huobi.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 
         // fcoin vs gateio
-        productApplication.diffTask(Constant.ETH_USDT, Gateio.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+//        productApplication.diffTask(Constant.ETH_USDT, Gateio.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 
         // fcoin vs dragonex
 //        productApplication.diffTask(Constant.LTC_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
         productApplication.diffTask(Constant.BTC_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
         productApplication.diffTask(Constant.BCH_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
+        productApplication.diffTask(Constant.EOS_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 2000);
 
 
         // double monitor
 //        productApplication.diffTask(Constant.ETH_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 1000);
         productApplication.diffTask(Constant.ETH_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 1000);
+
 
 //        productApplication.diffTask(Constant.XRP_USDT, Dragonex.PLATFORM_NAME, Fcoin.PLATFORM_NAME, 3000);
 
