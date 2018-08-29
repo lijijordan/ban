74531d00c3988a9f19a22b9fb2b2de53f1b3dd2b
liji <liji.jordan@gmail.com>
2018-08-29 09:50:48 +0800

    sockjs

--- src/main/java/com/jordan/ban/config/WebSocketConfig.java
+++ src/main/java/com/jordan/ban/config/WebSocketConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
 @Configuration
-public class WebMvcConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    /**
+     * Register Stomp endpoints: the url to open the WebSocket connection.
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
--- src/main/java/com/jordan/ban/controller/WebMsgHandler.java
+++
@@ -1,7 +0,0 @@
-package com.jordan.ban.controller;
-
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class WebMsgHandler extends TextWebSocketHandler {
-
-}
